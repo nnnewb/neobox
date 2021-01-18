80c618af4125da13d7763401c6d79ec298ea2ed3
weak_ptr <weak_ptr@outlook.com>
2021-01-18 17:08:28 +0800

    add basic search command

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ def list_(playlist):
                 ', '.join(map(lambda ar: ar.name, track.ar)),
                 f'{int((track.dt/1000)/60)}分{int((track.dt/1000)%60)}秒',
             )
-        
+
         rich.print(tbl)
 
     else:
@@ -103,8 +103,31 @@ def list_(playlist):
 
 
 @neobox.command('search', help='搜索歌曲', short_help='搜索歌曲')
-def search():
-    pass
+@click.argument('keywords')
+def search(keywords: str):
+    """搜索歌曲
+
+    Args:
+        keywords (str): 关键词，用逗号分割
+    """
+    search_keywords = ' '.join(keywords.split(','))
+    tbl = Table(title=f'搜索结果')
+    tbl.add_column('ID')
+    tbl.add_column('标题')
+    tbl.add_column('专辑')
+    tbl.add_column('歌手')
+    tbl.add_column('时长')
+
+    for song in api.search(search_keywords).result.songs:
+        tbl.add_row(
+            f'[bold green]{song.id}[/bold green]',
+            song.name,
+            song.album.name,
+            ', '.join(map(lambda ar: ar.name, song.artists)),
+            f'{int((song.duration/1000)/60)}分{int((song.duration/1000)%60)}秒',
+        )
+
+    rich.print(tbl)
 
 
 @neobox.command(help='播放歌曲', short_help='播放歌曲')
